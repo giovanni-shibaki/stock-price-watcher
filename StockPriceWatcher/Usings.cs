fdd2b1f12aa8b320702042cf1ecf07c6488f09af
giovanni-shibaki <giovannishibaki@gmail.com>
2023-10-02 14:18:34 -0300

    Initial Commit

---
+++ StockPriceWatcher/Usings.cs
@@ -0,0 +1,7 @@
+﻿// global using Class
+// Put all Global Usings here
+
+global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.IO;
+global using global::System.Threading;
